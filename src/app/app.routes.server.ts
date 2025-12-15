7a314f84c145936f70c976190411bdfd0a6343c0
Leonardo Cossio <leo15cossio@gmail.com>
2025-12-15 18:17:56 -0300

    organização para autenticação

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,42 +2,6 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'login',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'cadastro',
-    renderMode: RenderMode.Server
-  },
-   {
-    path: 'cadastro-curso',
-    renderMode: RenderMode.Server
-  },
-   {
-    path: 'cadastro-aula',
-    renderMode: RenderMode.Server
-  },
-   {
-    path: 'cadastro-modulo',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'avaliar-curso',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'cursos/**',
-    renderMode: RenderMode.Server
-  },
-   {
-    path: 'painel-curso',
-    renderMode: RenderMode.Server
-  },
-  {
     path: '**',
     renderMode: RenderMode.Server
   }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,49 +1,30 @@
 import { Routes } from '@angular/router';
-import { Courses } from './courses/courses/courses';
-import { Course } from './courses/model/course';
-import { CadastroCurso } from './courses/cadastro-curso/cadastro-curso';
-import { CadastroAula } from './courses/cadastro-aula/cadastro-aula';
-import { CadastroModulo } from './courses/cadastro-modulo/cadastro-modulo';
 import { Login } from './auth/login/login';
-import { CadastroUsuario} from './auth/cadastro-usuario/cadastro-usuario';
+import { CadastroUsuario } from './auth/cadastro-usuario/cadastro-usuario';
 import { TelaInicial } from './core/tela-inicial/tela-inicial';
 import { PainelAluno } from './aluno/painel-aluno/painel-aluno';
 import { PainelInstrutor } from './instrutor/painel-instrutor/painel-instrutor';
+import { authGuard, studentGuard, instructorGuard } from './auth/guards/auth.guard';
+import { CadastroCurso } from './courses/cadastro-curso/cadastro-curso';
+import { CadastroAula } from './courses/cadastro-aula/cadastro-aula';
+import { CadastroModulo } from './courses/cadastro-modulo/cadastro-modulo';
 import { AvaliacaoCurso } from './courses/avaliacao-curso/avaliacao-curso';
-import { PainelCurso } from './courses/painel-curso/painel-curso';
-import { PainelCursos } from './courses/painel-cursos/painel-cursos';
 
 
 export const routes: Routes = [
-
-  // Tela inicial
   { path: '', component: TelaInicial },
-
-  // Autenticação
   { path: 'login', component: Login },
   { path: 'cadastro', component: CadastroUsuario },
-
-  // Painéis principais
-  { path: 'aluno', component: PainelAluno },
-  { path: 'instrutor', component: PainelInstrutor },
-
-
-   // Painéis de curso
-  { path: 'cadastro-curso', component: CadastroCurso },
-  { path: 'cadastro-aula', component: CadastroAula },
-  { path: 'cadastro-modulo', component: CadastroModulo},
-  { path: 'avaliar-curso', component: AvaliacaoCurso },
-  { path: 'painel-curso', component: PainelCurso },
-  { path: 'painel-cursos', component: PainelCursos},
-
-  // Cursos (lazy loading)
+  { path: 'aluno', component: PainelAluno, canActivate: [authGuard, studentGuard] },
+  { path: 'instrutor', component: PainelInstrutor, canActivate: [authGuard, instructorGuard] },
+  { path: 'cadastro-curso', component: CadastroCurso, canActivate: [authGuard, instructorGuard] },
+  { path: 'cadastro-aula', component: CadastroAula, canActivate: [authGuard, instructorGuard] },
+  { path: 'cadastro-modulo', component: CadastroModulo, canActivate: [authGuard, instructorGuard] },
+  { path: 'avaliar-curso', component: AvaliacaoCurso, canActivate: [authGuard] },
   {
     path: 'cursos',
-    loadChildren: () =>
-      import('./courses/courses-module')
-        .then(m => m.CoursesModule)
+    canActivate: [authGuard],
+    loadChildren: () => import('./courses/courses-module').then(m => m.CoursesModule)
   },
-
-  // Fallback
   { path: '**', redirectTo: '' }
 ];
