6ef7cc582db6ca3f8da1c6dc94f5edea0db1bd7e
JAMS - João Moura <133531814+Jamsbr@users.noreply.github.com>
2025-12-14 22:51:42 -0300

    Pagina de cursos
    Prototipo inicial da pagina de cursos

--- src/app/courses/painel-cursos/painel-cursos.ts
+++ src/app/courses/painel-cursos/painel-cursos.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-painel-cursos',
-  imports: [],
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './painel-cursos.html',
   styleUrl: './painel-cursos.scss',
 })
