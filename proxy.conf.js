c9a7f9ddaab79eb2045448933faa7f2308f75530
Leonardo Cossio <leo15cossio@gmail.com>
2025-12-17 15:27:44 -0300

    proxy aceitando categorias

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,6 +1,6 @@
 const PROXY_CONFIG = [
   {
-    context: ['/auth', '/courses', '/modules', '/lessons', '/enrollments'],
+    context: ['/auth', '/courses', '/modules', '/lessons', '/enrollments', '/categories'],
     target: 'http://localhost:8080',
     secure: false,
     logLevel: 'debug',
